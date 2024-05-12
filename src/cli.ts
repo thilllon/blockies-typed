84adfd58b16774dc6f5658804da083d527932d3d
thilllon <thilllon970@gmail.com>
2024-05-12 14:41:13 +0900

    fix: logic error

--- src/cli.ts
+++ src/cli.ts
@@ -16,10 +16,7 @@ program
   .description('Generate blocky images')
   .option('-o, --output <output>', 'Output file', `${defaultSeed}.png`)
   .option('-s, --seed <seed>', 'Seed for random generation', defaultSeed)
-  .option('-n, --number <number>', 'Number of images to generate', '1')
   .parse(process.argv);
 
 const options = program.opts();
-for (let i = 0; i < Number(options.number); i++) {
-  writeFileSync(path.join(process.cwd(), options.output), createBuffer({ seed: options.seed }));
-}
+writeFileSync(path.join(process.cwd(), options.output), createBuffer({ seed: options.seed }));
